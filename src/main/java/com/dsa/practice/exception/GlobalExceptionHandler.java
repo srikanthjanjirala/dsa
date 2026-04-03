c8c859887560d7f0adc8e7c677361096197cbb73
srikanth <srikanthjanjirala26@gmail.com>
2026-04-03 11:15:07 +0530

    Change on pom.xml

--- src/main/java/com/dsa/practice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dsa/practice/exception/GlobalExceptionHandler.java
@@ -7,13 +7,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
